dbd910a46b29d124bb228ec4b2c76368c9d60a3f
Suraj Mishra <75450225+mishrasur7@users.noreply.github.com>
2021-03-04 10:26:49 +0200

    Add files via upload

---
+++ VIOPE Exercises/09.08/09.08LotteryEx.js
@@ -0,0 +1,35 @@
+	// Define the array variable and index variable inside the script element, but NOT inside the function. 
+	// Then they will be global and thus available for all functions to read from or write to.
+	var names = [];
+	var index = 0;
+	var outputText = ""; 
+
+
+	function addName() {
+		// Assign name from the input field to the array
+		var name = document.getElementById("name").value; 
+		names.push(name); 
+		var outputName = ""; 
+		// Increase the index by one
+		
+		// Go through the array in a for loop adding each name at the end of the namesText
+		
+		for (var i = 0; i < names.length; i++) {
+			outputName = outputName + names[i] + " "; 
+			outputText = "Inserted names: " + outputName;
+		}
+		// Write the names on the page, to the names div, as content of the div
+		
+		document.getElementById("nameList").innerHTML = outputText + "<br />"; 
+		// Empty the input field of name as that name was just put in the array
+		document.getElementById("name").value = ""; 
+		// Empty the answer text
+		document.getElementById("answer").innerHTML = ""; 
+	}
+
+	function makeDraw() {
+		// Randomize the index of the winner
+		var winningIndex = Math.floor(Math.random()*names.length); 
+		// Write the answer on the page, to the answer div, as content of the div
+		document.getElementById("answer").innerHTML = "Winner is " + names[winningIndex]; 
+	}
