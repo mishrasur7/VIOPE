809f1b07a60933c218ec5eca5f94c967755be574
Suraj Mishra <75450225+mishrasur7@users.noreply.github.com>
2021-03-10 15:38:11 +0200

    Add files via upload

---
+++ VIOPE Exercises/07.02/07.02ReturnValueEx.js
@@ -0,0 +1,7 @@
+// Insert your JavaScript code here
+function returnValue() {
+    var randomNumber = Math.random(); 
+    return randomNumber; 
+}
+var randomizeNumber = returnValue(); 
+console.log(randomizeNumber); 
